8994bfa749954d463a94a023bbbd619fa8f221b9
Richa <richamahajan1617@gmail.com>
2023-03-10 14:49:33 +0530

    Local storage in JS

---
+++ Storage/script.js
@@ -0,0 +1,26 @@
+const person = {
+    name: " Richa Mahajan",
+    location: "Mumbai",   
+}
+//setItem
+localStorage.setItem("user", JSON.stringify(person))
+
+//getItem
+console.log(JSON.parse(localStorage.getItem("user")))
+
+//key
+console.log(localStorage.key(0))
+
+//removeItem
+
+localStorage.removeItem("user")
+
+console.log(user)
+
+//clear
+
+
+localStorage.clear()
+
+//This method unlike the previous one will clear the entire localStorage.
+
